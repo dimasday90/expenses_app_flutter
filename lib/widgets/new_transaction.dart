00e08252554f2d775bd9feba0362f5643ed7f6bc
Dimas Agung Yudono <dimasday90@gmail.com>
2020-07-09 23:51:27 +0700

    add date picker

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addTransactionHandler;
@@ -10,54 +11,92 @@ class NewTransaction extends StatefulWidget {
 }
 
 class _NewTransactionState extends State<NewTransaction> {
-  final titleController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime _selectedDate;
 
-  final amountController = TextEditingController();
+  void _showDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(DateTime.now().year),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
+  }
 
-  void submitNewTransaction() {
-    final enteredTitle = titleController.text;
-    final enteredAmount = double.parse(amountController.text);
+  void _submitNewTransaction() {
+    final enteredTitle = _titleController.text;
+    final enteredAmount = double.parse(_amountController.text);
 
-    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+    if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectedDate == null) {
       return;
     }
 
-    widget.addTransactionHandler(enteredTitle, enteredAmount);
+    widget.addTransactionHandler(enteredTitle, enteredAmount, _selectedDate);
 
     Navigator.of(context).pop();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        elevation: 3,
-        child: Container(
-          padding: EdgeInsets.all(7.5),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              TextField(
-                decoration: InputDecoration(labelText: 'Title'),
-                controller: titleController,
-              ),
-              TextField(
-                decoration: InputDecoration(labelText: 'Amount'),
-                controller: amountController,
-                keyboardType: TextInputType.numberWithOptions(decimal: true),
-                onSubmitted: (_) => submitNewTransaction(),
-              ),
-              FlatButton(
-                child: Text(
-                  'Add Transaction',
-                  style: TextStyle(
-                      color: Colors.green,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold),
-                ),
-                onPressed: submitNewTransaction,
-              )
-            ],
+    return Container(
+      padding: EdgeInsets.all(8.5),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(labelText: 'Title'),
+            controller: _titleController,
           ),
-        ));
+          TextField(
+            decoration: InputDecoration(labelText: 'Amount'),
+            controller: _amountController,
+            keyboardType: TextInputType.numberWithOptions(decimal: true),
+          ),
+          Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      _selectedDate == null
+                          ? 'No Date Chosen'
+                          : 'Picked Date: ${DateFormat.yMMMd().format(_selectedDate)}',
+                    ),
+                  ),
+                  FlatButton(
+                    textColor: Theme.of(context).primaryColor,
+                    child: Text(
+                      'Choose Date',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    onPressed: _showDatePicker,
+                  )
+                ],
+              )),
+          RaisedButton(
+            color: Theme.of(context).primaryColor,
+            textColor: Theme.of(context).textTheme.button.color,
+            child: Text(
+              'Add Transaction',
+              style: TextStyle(
+                color: Theme.of(context).textTheme.button.color,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: _submitNewTransaction,
+          )
+        ],
+      ),
+    );
   }
 }
