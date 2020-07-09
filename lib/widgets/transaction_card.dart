b7c478e13fcbcb2ad1d6553508ba2cd141c97647
Dimas Agung Yudono <dimasday90@gmail.com>
2020-07-09 23:50:43 +0700

    transaction card widget updates - add 2 variables - convert card from Row to ListTile widget

--- lib/widgets/transaction_card.dart
+++ lib/widgets/transaction_card.dart
@@ -1,44 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+import '../models/amount.dart';
+
 class TransactionCard extends StatelessWidget {
+  final int id;
   final String title;
   final double amount;
   final DateTime date;
+  final Function deleteTransaction;
 
-  TransactionCard({this.title, this.amount, this.date});
+  TransactionCard(
+      {this.id, this.title, this.amount, this.date, this.deleteTransaction});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 4.0,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          Container(
-              margin: EdgeInsets.symmetric(vertical: 9.0, horizontal: 4.0),
-              padding: EdgeInsets.all(10.0),
-              decoration: BoxDecoration(
-                  border: Border.all(
-                      color: Theme.of(context).primaryColorLight, width: 3.0)),
-              child: Text(
-                '\$${amount.toStringAsFixed(2)}',
-                style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 19),
-              )),
-          Container(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(title,
-                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
-              Text(DateFormat.yMd().add_jm().format(date),
-                  style: TextStyle(color: Colors.grey))
-            ],
-          ))
-        ],
+      margin: EdgeInsets.symmetric(
+        vertical: 7,
+        horizontal: 4,
+      ),
+      elevation: 5,
+      child: ListTile(
+        leading: CircleAvatar(
+          radius: 30,
+          child: Padding(
+            padding: EdgeInsets.all(6),
+            child: FittedBox(
+              child: Text(Amount(amount, 2).formated,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 18,
+                  )),
+            ),
+          ),
+        ),
+        title: Text(
+          title,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 17,
+          ),
+        ),
+        subtitle: Text(DateFormat.yMMMd().format(date)),
+        trailing: IconButton(
+          icon: Icon(Icons.delete),
+          color: Theme.of(context).errorColor,
+          onPressed: () => deleteTransaction(id),
+        ),
       ),
     );
   }
