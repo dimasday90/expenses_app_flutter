af9c60d8a34905c7bb5592ee8472c0e9f1c4bef4
Dimas Agung Yudono <dimasday90@gmail.com>
2020-07-09 23:47:45 +0700

    transaction wiget updates - add new funcion variable -  add condition for rendering transactions list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -5,28 +5,55 @@ import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
+  final Function deleteTransaction;
 
-  TransactionList({this.transactions});
+  TransactionList({this.transactions, this.deleteTransaction});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.symmetric(vertical: 8.9),
-      height: 308,
+      height: 278,
+      width: double.infinity,
       padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 9.8),
       decoration: BoxDecoration(
-          border: Border.all(
-              color: Theme.of(context).primaryColorDark, width: 2.1)),
-      child: ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (ctx, index) {
-          return TransactionCard(
-            title: transactions[index].title,
-            amount: transactions[index].amount,
-            date: transactions[index].date,
-          );
-        },
+        border: Border.all(
+          color: Theme.of(context).primaryColorDark,
+          width: 2.1,
+        ),
       ),
+      child: transactions.isEmpty
+          ? Column(
+              children: <Widget>[
+                Container(
+                  height: 210,
+                  margin: EdgeInsets.symmetric(vertical: 8.1),
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.scaleDown,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 8.1),
+                  child: Text('Transaction is empty',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                      )),
+                ),
+              ],
+            )
+          : ListView.builder(
+              itemCount: transactions.length,
+              itemBuilder: (ctx, index) {
+                return TransactionCard(
+                  id: transactions[index].id,
+                  title: transactions[index].title,
+                  amount: transactions[index].amount,
+                  date: transactions[index].date,
+                  deleteTransaction: deleteTransaction,
+                );
+              },
+            ),
     );
   }
 }
