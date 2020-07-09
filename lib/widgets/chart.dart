ace2321a7c7e6ceee8a8c2042190796a844f2e43
Dimas Agung Yudono <dimasday90@gmail.com>
2020-07-09 23:52:59 +0700

    add widgets to show amount chart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,13 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
+import './chart_bar.dart';
 
 class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart({this.recentTransactions});
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+      double totalSum = 0.0;
+
+      for (int i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+
+      return {
+        'day': DateFormat.E().format(weekDay),
+        'amount': totalSum,
+      };
+    }).reversed.toList();
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (acc, item) {
+      return acc + item['amount'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(5.0),
       width: double.infinity,
       child: Card(
-        child: Text('Chart'),
+        elevation: 6,
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: groupedTransactionValues.map((tx) {
+                  return Flexible(
+                    fit: FlexFit.tight,
+                    child: ChartBar(
+                      label: tx['day'],
+                      spendingAmount: tx['amount'],
+                      spendingPctOfTotal:
+                          (tx['amount'] as double) / totalSpending,
+                    ),
+                  );
+                }).toList(),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                'Last Week Chart',
+                style: TextStyle(
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
